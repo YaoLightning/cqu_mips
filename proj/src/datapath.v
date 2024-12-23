619a3a37473b031850a262ce32f3fd2ffccb27d3
ylgg01 <154301480+ylgg01@users.noreply.github.com>
2024-12-23 11:28:50 +0800

    init project

---
+++ proj/src/datapath.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2024/12/23 11:18:06
+// Design Name: 
+// Module Name: datapath
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module datapath(
+    input clk, rst
+    );
+endmodule
